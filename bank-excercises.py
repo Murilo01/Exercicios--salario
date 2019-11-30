d50ba255eb2141d424feff1b7908c48d6b16d615
Murilo Eustaquio Duarte <muriloduarte722@gmail.com>
2019-11-30 19:09:31 -0300

    Exercicios 27/11

--- bank-excercises.py
+++ bank-excercises.py
@@ -1,27 +1,22 @@
 class Dados_bancarios:
-    def __init__(self, nome, idade, saque):
-        nome.self = nome
-        idade.self = idade
-        saque.self = saque
+    def __init__(self, nome, idade, saque, deposito, emprestimo, visualizar_informacoes):
+        self. nome = nome
+        self. idade = idade
+        self. saque = saque
+        self. deposito = deposito
+        self. emprestimo = emprestimo
+        self. visualizar_informacoes = visualizar_informacoes
 
-    def dados_bancarios(self, nome, idade, saque):
-        nome = input("Digite seu nome completo"),
-        idade = int(input("Digite sua idade")),
-        saque = float(input("Digite o valor do saque"))
+dados_bancarios = Dados_bancarios(
+    nome = input("Digite seu nome completo"),
+    idade = int(input("Digite a idade")),
+    saque = float(input("Digite o valor do saque")),
+    deposito = float(input("Digite o valor do depósito")),
+    emprestimo = float(input("Digite o valor do empréstimo")),
+    visualizar_informacoes = Dados_bancarios
+)
 
-class Options:
-    def __init__(self, saque, deposito, emprestimo, visualizar_informacoes):
-        saque.self = saque
-        deposito.self = deposito
-        emprestimo.self = emprestimo
-        visualizar_informacoes.self = visualizar_informacoes
-
-    def informacoes(saque, deposito, emprestimo, visualizar_informacoes):
-        saque = "Digite" - 1 - "para saque"
-        deposito = "Digite" - 2 - "para depoósito"
-        emprestimo = "Digite" - 3 - "para empréstimo"
-        visualizar_informacoes = "Digite" - 4 - "para visualizar as informações"
-print()
+print(dados_bancarios)
 
 
 
