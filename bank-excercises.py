18b11105c7eb1ecb89bd1a7e318332e376995b25
Murilo Eustaquio Duarte <muriloduarte722@gmail.com>
2019-11-30 17:03:10 -0300

    Exercicios 27/11

---
+++ bank-excercises.py
@@ -0,0 +1,28 @@
+class Dados_bancarios:
+    def __init__(self, nome, idade, saque):
+        nome.self = nome
+        idade.self = idade
+        saque.self = saque
+
+    def dados_bancarios(self, nome, idade, saque):
+        nome = input("Digite seu nome completo"),
+        idade = int(input("Digite sua idade")),
+        saque = float(input("Digite o valor do saque"))
+
+class Options:
+    def __init__(self, saque, deposito, emprestimo, visualizar_informacoes):
+        saque.self = saque
+        deposito.self = deposito
+        emprestimo.self = emprestimo
+        visualizar_informacoes.self = visualizar_informacoes
+
+    def informacoes(saque, deposito, emprestimo, visualizar_informacoes):
+        saque = "Digite" - 1 - "para saque"
+        deposito = "Digite" - 2 - "para depoósito"
+        emprestimo = "Digite" - 3 - "para empréstimo"
+        visualizar_informacoes = "Digite" - 4 - "para visualizar as informações"
+print()
+
+
+
+
