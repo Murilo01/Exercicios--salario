42d151f9b5102bc05cf59f18cfcc65e2765dcfa1
Murilo Eustaquio <muriloduarte722@gmail.comgit config user.name Murilo>
2019-12-12 14:29:13 -0300

    Python---Exercicios---26/11

--- exercicios-26/1-numero_inteiro.py
+++ exercicios-26/1-numero_inteiro.py
@@ -1,5 +1,15 @@
-def n_i(x):
-    x.__mod__(x)
+class Numero_inteiro:
+
+    def __mod__(self, impar, par):
+        impar.self = impar
+        par.self = par
+
+impar = int(input("Digite um numero")),
+par = int(input("Digite um numero"))
+
+
+
+
 
 
 #x = int(input("digite um numero"))
